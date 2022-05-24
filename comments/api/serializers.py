1249ae6d579b851b17c71ee4763c5ed510689bc2
jing-wu-lu <wujing567@gmail.com>
2022-05-24 09:56:19 -0500

    Comment api for destroy and update

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -43,3 +43,17 @@ class CommentSerializerForCreate(serializers.ModelSerializer):
             tweet_id=validated_data['tweet_id'],
             content=validated_data['content'],
         )
+
+
+class CommentSerializerForUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('content',)
+
+    def update(self, instance, validated_data):
+        instance.content = validated_data['content']
+        instance.save()
+        # update requires modified instance to be returned
+        return instance
+
+
