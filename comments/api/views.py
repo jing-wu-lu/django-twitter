d408358a1e17bf599d10995c1a3d85f14c13241f
jing-wu-lu <wujing567@gmail.com>
2022-05-28 15:15:25 -0500

    Install notifications and add api

--- comments/api/views.py
+++ comments/api/views.py
@@ -9,6 +9,7 @@ from comments.api.serializers import (
 )
 from comments.api.permissions import IsObjectOwner
 from utils.decorators import required_params
+from inbox.services import NotificationService
 
 
 class CommentViewSet(viewsets.GenericViewSet):
@@ -78,6 +79,7 @@ class CommentViewSet(viewsets.GenericViewSet):
             }, status=status.HTTP_400_BAD_REQUEST)
         # save 方法会触发 serializer 里的 create 方法， 点进 save 的具体实现可以看到
         comment = serializer.save()
+        NotificationService.send_comment_notification(comment)
         return Response(
             CommentSerializer(comment, context={'request': request}).data,
             status=status.HTTP_201_CREATED,
