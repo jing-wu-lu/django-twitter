df678f45f1db798f7779756b0efa344479b3a673
jing-wu-lu <wujing567@gmail.com>
2022-06-15 15:36:03 -0500

    Revert "add tweets pagination"
    This reverts commit 865e8c87d32b2521994370f5f7575f55a9b906a4.

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,6 +1,6 @@
+#from django.contrib.auth.models import User
 from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
-from friendships.services import FriendshipService
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -8,36 +8,21 @@ from rest_framework.exceptions import ValidationError
 # 可以通过 source = xxx 指定去访问每个model instance 的 xxx 方法
 # 即 model_instance.xxx 来获取数据
 #  https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
-
-
 class FollowerSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='from_user')
-    has_followed = serializers.SerializerMethodField()
     # created_at = serializers.DateTimeField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at', 'has_followed')
-
-    def get_has_followed(self, obj):
-        if self.context['request'].user.is_anonymous:
-            return False
-        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询， 速度会很慢，如何优化呢？
-        return FriendshipService.has_followed(self.context['request'].user, obj.from_user)
+        fields = ('user', 'created_at')
 
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='to_user')
-    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at', 'has_followed')
-
-    def get_has_followed(self, obj):
-        if self.context['request'].user.is_anonymous:
-            return False
-        return FriendshipService.has_followed(self.context['request'].user, obj.to_user)
+        fields = ('user', 'created_at')
 
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
--- friendships/api/views.py
+++ friendships/api/views.py
@@ -9,39 +9,30 @@ from friendships.api.serializers import (
     FriendshipSerializerForCreate,
 )
 from django.contrib.auth.models import User
-from utils.paginations import FriendshipPagination
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
     serializer_class = FriendshipSerializerForCreate
     queryset = User.objects.all()
-    # 一般来说，不同的 views 所需要的 pagination 规则肯定是不同的，因此一般都需要自定义
-    pagination_class = FriendshipPagination
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followers(self, request, pk):
         # GET /api/friendships/1/followers
         friendships = Friendship.objects.filter(to_user_id=pk)
-        # serializer = FollowerSerializer(friendships, many=True)
-        # return Response(
-        #     {'followers': serializer.data},
-        #     status=status.HTTP_200_OK,
-        # )
-        page = self.paginate_queryset(friendships)
-        serializer = FollowerSerializer(page, many=True, context={'request': request})
-        return self.get_paginated_response(serializer.data)
+        serializer = FollowerSerializer(friendships, many=True)
+        return Response(
+            {'followers': serializer.data},
+            status=status.HTTP_200_OK,
+        )
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id=pk)
-        # serializer = FollowingSerializer(friendships, many=True)
-        # return Response(
-        #     {'followings': serializer.data},
-        #     status=status.HTTP_200_OK,
-        # )
-        page = self.paginate_queryset(friendships)
-        serializer = FollowingSerializer(page, many=True, context={'request': request})
-        return self.get_paginated_response(serializer.data)
+        serializer = FollowingSerializer(friendships, many=True)
+        return Response(
+            {'followings': serializer.data},
+            status=status.HTTP_200_OK,
+        )
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
@@ -62,7 +53,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
 
         instance = serializer.save()
         return Response(
-            FollowingSerializer(instance, context={'request': request}).data,
+            FollowingSerializer(instance).data,
             status=status.HTTP_201_CREATED,
         )
 
