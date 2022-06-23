c99a28eb42c9759960debfb4c762cc1373d49da8
jing-wu-lu <wujing567@gmail.com>
2022-06-23 17:07:13 -0500

    add newsfeeds in redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from newsfeeds.api.serializers import NewsFeedSerializer
 from utils.paginations import EndlessPagination
 
@@ -10,12 +9,12 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = EndlessPagination
 
-    def get_queryset(self):
-        # self define queryset, because need permission to view newsfeeds
-        # only view the current logged in user's newsfeed
-        # also can be self.request.user.newsfeed_set.all()
-        # it is the best to write as NewsFeed.objects.filter(user=self.request.user)
-        return NewsFeed.objects.filter(user=self.request.user)
+    # def get_queryset(self):
+    #     self define queryset, because need permission to view newsfeeds
+    #     only view the current logged-in user's newsfeed
+    #     also can be self.request.user.newsfeed_set.all()
+    #     it is the best to write as NewsFeed.objects.filter(user=self.request.user)
+    #     return NewsFeed.objects.filter(user=self.request.user)
 
     def list(self, request):
         # serializer = NewsFeedSerializer(
@@ -26,7 +25,8 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
         # return Response({
         #     'newsfeeds': serializer.data,
         # }, status=status.HTTP_200_OK)
-        page = self.paginate_queryset(self.get_queryset())
+        queryset = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginate_queryset(queryset)
         serializer = NewsFeedSerializer(
             page,
             context={'request': request},
